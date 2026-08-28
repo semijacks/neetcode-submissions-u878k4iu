0bbb5f1a1809169aec23bc965390b75ec8e9a7c9
Semilore Idowu <semijacks27@gmail.com>
2026-08-28 23:48:26 +0100

    Add: divide-array-into-equal-pairs - submission-0

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        odd_set = set()
+
+        for num in nums:
+            if num not in odd_set:
+                odd_set.add(num)
+            else:
+                odd_set.remove(num)
+
+        return not len(odd_set)
